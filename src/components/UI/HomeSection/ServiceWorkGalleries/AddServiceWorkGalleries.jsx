bc0737b5b375f2edec9d919d5d2eaaa3ba4b635c
zamirul <javshazib@gmail.com>
2026-02-07 16:09:28 +0600

    ServiceWorkGalleries added done

---
+++ src/components/UI/HomeSection/ServiceWorkGalleries/AddServiceWorkGalleries.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddServiceWorkGalleries = () => {
+  return <div></div>;
+};
+
+export default AddServiceWorkGalleries;
---
+++ src/components/UI/HomeSection/ServiceWorkGalleries/EditServiceWorkGalleries.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditServiceWorkGalleries = () => {
+  return <div></div>;
+};
+
+export default EditServiceWorkGalleries;
