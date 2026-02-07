c09628d2935e612d529b023011b1fa9c61a46447
zamirul <javshazib@gmail.com>
2026-02-07 23:37:14 +0600

    QuestionAnswers added done

---
+++ src/app/(withDashboard)/home-page/question-answers/all-question-answers/page.jsx
@@ -0,0 +1,12 @@
+import AllQuestionAnswers from "@/components/UI/HomeSection/QuestionAnswers/AllQuestionAnswers";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AllQuestionAnswers />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/UI/HomeSection/QuestionAnswers/EditQuestionAnswers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditQuestionAnswers = () => {
+  return <div></div>;
+};
+
+export default EditQuestionAnswers;
