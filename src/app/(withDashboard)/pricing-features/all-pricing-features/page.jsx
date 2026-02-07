29f6330099e04d690e8d60b58f03608da86f3e5e
zamirul <javshazib@gmail.com>
2026-02-07 23:22:31 +0600

    pricing-features added done

---
+++ src/app/(withDashboard)/pricing-features/all-pricing-features/page.jsx
@@ -0,0 +1,12 @@
+import AllPricingFeatures from "@/components/UI/PricingFeatures/AllPricingFeatures";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AllPricingFeatures />
+    </div>
+  );
+};
+
+export default page;
