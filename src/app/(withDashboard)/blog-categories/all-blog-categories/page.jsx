d6aabd738cb4cbc69c797fcf4945c7c18fc35534
zamirul <javshazib@gmail.com>
2026-02-07 16:33:47 +0600

    BlogCategories added done

---
+++ src/app/(withDashboard)/blog-categories/all-blog-categories/page.jsx
@@ -0,0 +1,12 @@
+import AllBlogCategories from "@/components/UI/Blog/BlogCategories/AllBlogCategories";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AllBlogCategories />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/UI/Blog/BlogCategories/AllBlogCategories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllBlogCategories = () => {
+  return <div></div>;
+};
+
+export default AllBlogCategories;
