21b47b7ac91f701b4cf1abec83f104eee87994f9
zamirul <javshazib@gmail.com>
2026-02-07 23:55:38 +0600

    why-choose-us added done

---
+++ src/app/(withDashboard)/why-choose-us/all-why-choose-us/page.jsx
@@ -0,0 +1,12 @@
+import AllWhyChooseUs from "@/components/UI/WhyChooseUs/AllWhyChooseUs";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AllWhyChooseUs />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/UI/WhyChooseUs/AddWhyChooseUs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddWhyChooseUs = () => {
+  return <div></div>;
+};
+
+export default AddWhyChooseUs;
