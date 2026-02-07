878a89f2e7195c8ed75b943a45737530102f17b5
zamirul <javshazib@gmail.com>
2026-02-07 18:49:22 +0600

    Blogs added done

---
+++ src/app/(withDashboard)/blogs/add-blogs/page.jsx
@@ -0,0 +1,12 @@
+import AddBlogs from "@/components/UI/Blog/Blogs/AddBlogs";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AddBlogs />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/UI/Blog/Blogs/EditBlogs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditBlogs = () => {
+  return <div></div>;
+};
+
+export default EditBlogs;
