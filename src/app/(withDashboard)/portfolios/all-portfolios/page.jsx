feb2f6bf957909bab1dee660a93fd97a5288d949
zamirul <javshazib@gmail.com>
2026-02-07 23:06:03 +0600

    Portfolios added done

---
+++ src/app/(withDashboard)/portfolios/all-portfolios/page.jsx
@@ -0,0 +1,12 @@
+import AllPortfolios from "@/components/UI/Portfolios/AllPortfolios";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <AllPortfolios />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/UI/Portfolios/AddPortfolios.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddPortfolios = () => {
+  return <div></div>;
+};
+
+export default AddPortfolios;
